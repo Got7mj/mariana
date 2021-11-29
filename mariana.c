5d5fed47465acf3ae86717ba18dd3552093c0edb
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-11-29 10:23:38 -0300

    Update mariana.c

--- mariana.c
+++ mariana.c
@@ -114,3 +114,33 @@ void telaSobre(void) {
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
+
+
+
+void telaEquipe(void) {
+    sistema("limpar||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///                   Universidade Federal do Rio Grande do Norte                     ///\n");
+    printf("///                   Centro de Ensino Superior do Seridó                             ///\n");
+    printf("///                   Departamento de Computação e Tecnologia                         ///\n");
+    printf("///                   Disciplina DCT1106 - Programação                                ///\n");
+    printf("///                   Projeto Sistema de Planejamento de Dieta                        ///\n");
+    printf("///                   Desenvolvido por mariana - 2021.2                               ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///             = = = = = = Sistema de Planejamento de Dieta = = = = = =              ///\n");
+    printf("///                                                                                   ///\n");
+    printf("///             Este projeto exemplo foi desenvolvido por:                            ///\n");
+    printf("///                                                                                   ///\n");
+    printf("///             Mariana Araújo de Medeiros                                            ///\n");
+    printf("///             E-mail: mariana.medeiros.017@ufrn.edu.br                              ///\n");
+    printf("///             Repositório: https://github.com/ Got7mj/mariana                       ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
