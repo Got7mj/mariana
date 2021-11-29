5b0753bda5634f3a23dfb888fb36ba558cceb0cc
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-11-29 13:40:50 -0300

    Create  receita.h

---
+++ receita.h
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                   ///
+///                   Universidade Federal do Rio Grande do Norte                     ///
+///                   Centro de Ensino Superior do Seridó                             ///
+///                   Departamento de Computação e Tecnologia                         ///
+///                   Disciplina DCT1106 - Programação                                ///
+///                   Projeto Sistema de Planejamento de Dieta                        ///
+///                   Desenvolvido por mariana                                        ///
+///                                                                                   ///
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//////
+////// Assistenaturas das funções 
+//////
+
+void moduloReceita(void);
+char telaMenuReceita(void);
+void telaCadastrarReceita(void);
+void telaPesquisarRceita(void);
+void telaAlterarRceita(void);
+void telaExcluirRceita(void);
