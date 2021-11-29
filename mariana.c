7bf8c415d8b0cfe8a8b759554fce679edb5a6c30
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-11-29 10:00:46 -0300

    Update mariana.c

--- mariana.c
+++ mariana.c
@@ -18,3 +18,36 @@ void telaEquipe(void);
 //////
 ////// Programa principal
 //////
+
+int main(void) {
+  char opcao;
+  
+  do {
+     opcao = telaPrincipal();
+     switch(opcao) {
+         case '1':   moduloCardápio();
+                     break;
+         case '2':   moduloPaciente();
+                     break;
+         case '3':   moduloReceita();
+                     break;
+         case '4':   // Módulo Relatórios
+                     break;
+         case '5':   telaSobre();
+                     tela Principal();
+                     break;
+     }
+  } while (opcao != '0');
+  
+  return 0;
+}
+
+
+
+//////
+////// Funções do Módulo Principal
+//////
+
+
+
+char telaPrincipal(void) {
