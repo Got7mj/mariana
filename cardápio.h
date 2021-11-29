9d86cafbf735f876dc87a5db6bddfc42eafc66ab
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-11-29 13:04:56 -0300

    Create cardápio.h

---
+++ cardápio.h
@@ -0,0 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                   ///
+///                   Universidade Federal do Rio Grande do Norte                     ///
+///                   Centro de Ensino Superior do Seridó                             ///
+///                   Departamento de Computação e Tecnologia                         ///
+///                   Disciplina DCT1106 - Programação                                ///
+///                   Projeto Sistema de Planejamento de Dieta                        ///
+///                   Desenvolvido por mariana                                        ///
+///                                                                                   ///
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+//////
+////// Assisnaturas das funcões Módulo Cardápio
+//////
+
+void moduloCardápio(void);
+char telaMenuCardápio(void);
+void telaCadastrarCardápio(void);
+void telaPesquisarCardápio(void);
+void telaAlterarCardápio(void);
+void telaExcluirCardápio(void);
