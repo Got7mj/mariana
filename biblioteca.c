b0fc7e2498a10c3c21c26baea798de06778db4b4
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-12-06 20:11:53 -0300

    Update biblioteca.c

--- biblioteca.c
+++ biblioteca.c
@@ -57,6 +57,46 @@ int ehLetra(char c) {
 
 
 ///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se ano for bissexto (divisível por 4, não divisível por ...
+/// 100 ou divisível por 400) e retorna 0 caso contrário
+///
+int ehBissexto(int aa) {
+  if ((aa % 4 == 0) && (aa % 100 != 0)) {
+    return 1;
+  } else if (aa % 400 == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se dia, mes e ano correspondem a uma data válida, inclusive
+/// em anos bissextos, ou retorna 0 caso contrário
+///
+int ehData(int dd, int mm, int aa) {
+  int maiorDia;
+  if (aa < 0 || mm < 1 || mm > 12)
+    return 0;
+  if (mm == 2) {
+    if (ehBissexto(aa)) 
+      maiorDia = 29;
+    else
+      maiorDia = 28;
+  } else if (mm == 4 || mm == 6 || mm == 9 || mm == 11) {
+    maiorDia = 30;
+  } else
+    maiorDia = 31;
+  if (dd < 1 || dd > maiorDia)
+    return 0;
+  return 1;
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////
 /// Retorna 1 se string recebido for exclusivamente alfabético ou
 /// retorna 0 caso contrário
 ///
@@ -103,3 +143,23 @@ int validarData(char* data) {
 int validarcelular(char* celular) {
 	return 1;
 } 
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+/// Retorna 1 se string recebido corresponder a um número da numeração válido 
+/// (apenas dígitos) ou retorna 0 caso contrário
+///
+int validarNum(char* num) {
+  int tam;
+  tam = strlen(num);
+  if (tam < 10 || tam > 11) {
+    return 0;
+  }
+  for (int i = 0; i < tam; i++) {
+    if (!ehDigito(num[i])) {
+      return 0;
+    }
+  }
+  return 1;
+}
