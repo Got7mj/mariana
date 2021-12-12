708a6ba394cd0adf3ccd9c1414cb1208a1f89ae7
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-12-12 09:36:31 -0300

    Update cardápio.c

--- cardápio.c
+++ cardápio.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unist.h>
+#include <string.h>
 #include "aluno.h"
-
-
-
-///////
-//////Funções do Módulo Cardápio
-//////
+#include "biblioteca.h"
 
 void moduloCardápio(void) {
     char opção;
     do {
-         opção = telaMenuCardápio();
+         opção = menuCardápio();
          switch(opção) {
              case '1':  telaCadastrarCardápio();
                         break;
@@ -28,64 +23,92 @@ void moduloCardápio(void) {
 
 
 
-void telaMenuCardápio(void) {
-    char op;
-    
-    system("clear||cls")
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                               ///\n");
-    printf("///                 = = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
-    printf("///               = = = = = = = = = = Menu Cardápio = = = = = = = = = =           ///\n");
-    printf("///                = = = = = = = = = = = = = = = = = = = = = = = = = =            ///\n");
-    printf("///               = = = = = = = = = = Menu Cardápio = = = = = = = = = =           ///\n");
-    printf("///                = = = = = = = = = = = = = = = = = = = = = = = = = =            ///\n");
-    printf("///                                                                               ///\n");
-    printf("///                1. Cadastrar um novo Cardápio                                  ///\n");
-    printf("///                2. Pesquisar a numeração de um Cardápio                        ///\n");
-    printf("///                3. Atualizar o cadastro do Cardápio                            ///\n");
-    printf("///                4. Excluir um Cardápio do sistema                              ///\n");
-    printf("///                0. Voltar ao menu anterior                                     ///\n");
-    printf("///                                                                               ///\n");
-    printf("///                Escolha a opção desejada: 
-    scanf("%c", &op);
-    getchar(); 
-    printf("///                                                                               ///\n");
-    printf("///                                                                               ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n")
-    printf("\t\t\t<<< ... Aguarde ... >>>\n");
-    sleep(1);
-    return op;
+void cadastrarCardápio(void) {
+        // função ainda em desenvolvimento
+        // exibe a tela apenas para testes
+        telaCadastrarCardápio();
+}
+
+
+void pesquisarCardápio(void) {
+        // função ainda em desenvolvimento
+        // exibe a tela apenas para testes
+        telaPesquisarCardápio();
+}
+
+
+void alterarCardápio(void) {
+        // função ainda em desenvolvimento
+        // exibe a tela apenas para testes
+        telaAlterarCardápio();
+}
+
+
+void excluirCardápio(void) {
+        // função ainda em desenvolvimento
+        // exibe a tela apenas para testes
+        void telaExcluirCardápio();
+}
+
+
+char menuCardápio(void) {
+        char op;
+    sistema("clear||cls");
+        printf("\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                               ///\n");
+        printf("///                 = = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+        printf("///               = = = = = = = = = = Menu Cardápio = = = = = = = = = =           ///\n");
+        printf("///                = = = = = = = = = = = = = = = = = = = = = = = = = =            ///\n");
+        printf("///               = = = = = = = = = = Menu Cardápio = = = = = = = = = =           ///\n");
+        printf("///                = = = = = = = = = = = = = = = = = = = = = = = = = =            ///\n");
+        printf("///                                                                               ///\n");
+        printf("///                1. Cadastrar um novo Cardápio                                  ///\n");
+        printf("///                2. Pesquisar a numeração de um Cardápio                        ///\n");
+        printf("///                3. Atualizar o cadastro do Cardápio                            ///\n");
+        printf("///                4. Excluir um Cardápio do sistema                              ///\n");
+        printf("///                0. Voltar ao menu anterior                                     ///\n");
+        printf("///                                                                               ///\n");
+        printf("///                Escolha a opção desejada: 
+        scanf("%c", &op);
+        getchar(); 
+        printf("///                                                                               ///\n");
+        printf("///                                                                               ///\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        delay(1);
+        return op;
 }
 
            
  
 void telaCadastrarCardápio(void) {
-    char num[12];
-    char criação[11];
+        char num[12];
+        char criação[11];
     
     sistema("clear||cls");
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                               ///\n");
-    printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                = = = = = = = = = Cadastrar Cardápio = = = = = = = =           ///\n");
-    printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                               ///\n");
-    printf("///                  Numeração (apenas números): ");  
-    scanf("%[0-9]", num);
-    getchar();
-    printf("///                  Data da Criação (dd/mm/aaaa): ");
-    scanf("%[0-9]", criação);
-    getchar();
-    printf("///                                                                               ///\n");
-    printf("///                                                                               ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t<<< ... Aguarde ... >>>\n");
-    sleep(1);
-    return op;
+        printf("\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                               ///\n");
+        printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+        printf("///                = = = = = = = = = Cadastrar Cardápio = = = = = = = =           ///\n");
+        printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+        printf("///                                                                               ///\n");
+    do {
+            printf("///               Numeração (apenas números): ");  
+        scanf("%[^\n]", num);
+        getchar();
+    } while (!validaNum(num));
+    } while (!validarNum(num));
+        printf("///                  Data da Criação (dd/mm/aaaa): ");
+        scanf("%[0-9]", criação);
+        getchar();
+    do {
+        printf("///                                                                               ///\n");
+        printf("///                                                                               ///\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        delay(1);
 }
 
            
@@ -94,23 +117,21 @@ void telaPesquisarCardápio(void) {
     char num[12];
     
     sistema("clear||cls");
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                               ///\n");
-    printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
-    printf("///               = = = = = = = = Pesquisar Cardápio = = = = = = = =              ///\n");
-    printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
-    printf("///                                                                               ///\n");
-    printf("///                 Informe a numeração (apenas números): 
-    scanf("%[0-9]", num);
-    getchar();
-    printf("///                                                                               ///\n");
-    printf("///                                                                               ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t<<< ... Aguarde ... >>>\n");
-    sleep(1);
-    return op;
+        printf("\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                               ///\n");
+        printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
+        printf("///               = = = = = = = = Pesquisar Cardápio = = = = = = = =              ///\n");
+        printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
+        printf("///                                                                               ///\n");
+        printf("///                 Informe a numeração (apenas números): 
+        scanf("%[0-9]", num);
+        getchar();
+        printf("///                                                                               ///\n");
+        printf("///                                                                               ///\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        delay(1);
 }       
            
 
@@ -119,23 +140,21 @@ void telaAlterarCardápio(void) {
     char num[12];
     
     sistema("clear||cls");
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                               ///\n");
-    printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                = = = = = = = = Alterar Cardápio = = = = = = = = =             ///\n");
-    printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
-    printf("///                                                                               ///\n");
-    printf("///                  Informe a numeração(apenas números): 
-    scanf("%[0-9]", num);
-    getchar();
-    printf("///                                                                               ///\n");
-    printf("///                                                                               ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t<<< ... Aguarde ... >>>\n");
-    sleep(1);
-    return op;
+        printf("\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                               ///\n");
+        printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+        printf("///                = = = = = = = = Alterar Cardápio = = = = = = = = =             ///\n");
+        printf("///                  = = = = = = = = = = = = = = = = = = = = = = = =              ///\n");
+        printf("///                                                                               ///\n");
+        printf("///                  Informe a numeração(apenas números): 
+        scanf("%[0-9]", num);
+        getchar();
+        printf("///                                                                               ///\n");
+        printf("///                                                                               ///\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        delay(1);
 }       
 
 
@@ -144,23 +163,90 @@ void telaExcluirCardápio(void) {
     char num[12];
     
     sistema("clear||cls");
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                               ///\n");
-    printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
-    printf("///               = = = = = = = = Excluir Cardápio = = = = = = = = =              ///\n");
-    printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
-    printf("///                                                                               ///\n");
-    printf("///                 Informe a numeração (apenas números): 
-    scanf("%[0-9]", num);
-    getchar();
-    printf("///                                                                               ///\n");
-    printf("///                                                                               ///\n");
-    printf("/////////////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("\t\t\t<<< ... Aguarde ... >>>\n");
-    sleep(1);
-    return op;
+        printf("\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("///                                                                               ///\n");
+        printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
+        printf("///               = = = = = = = = Excluir Cardápio = = = = = = = = =              ///\n");
+        printf("///                 = = = = = = = = = = = = = = = = = = = = = = = =               ///\n");
+        printf("///                                                                               ///\n");
+        printf("///                 Informe a numeração (apenas números): 
+        scanf("%[0-9]", num);
+        getchar();
+        printf("///                                                                               ///\n");
+        printf("///                                                                               ///\n");
+        printf("/////////////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        delay(1);
 }       
-           
+ 
+               
+             
+///
+/// Funções de validação - serão transferidas posteriormente para o módulo biblioteca.c
+///
+
+int validaNome(char nome[]) {
+
+}
+
+
+
+
+               
+ int validaEmail(char email[]) {
+
+}
+
+
+int validaCPF(char cpf[]) {
+    int tam;
     
+    tam = strlen(cpf);
+    if (tam != 11) {
+        return 0;
+    }
+    for (int i = 0; i < tam; i++) {
+        if (!ehDigito(cpf[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}   
+
+               
+int validaData(char data[]) {
+
+}
+
+
+int validaCelular(char cel[]) {
+    int tam;
+
+    tam = strlen(cel);
+    if (tam != 11) {
+        return 0;
+    }
+    for (int i = 0; i < tam; i++) {
+        if (!ehDigito(cel[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}   
+
+             
+int validaNum(char num[]) {
+    int tam;
+
+    tam = strlen(num);
+    if (tam < 1000 || tam > 1001) {
+        return 0;
+    }
+    for (int i = 0; i < tam; i++) {
+        if (!ehDigito(num[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
