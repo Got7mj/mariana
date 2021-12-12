f7949b8b23e81b9329b859259159bdf11bb584f9
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-12-12 11:05:44 -0300

    Create relatorio.h

---
+++ relatorio.h
@@ -0,0 +1,13 @@
+void moduloCardapio(void);
+char menuCardapio(void);
+
+void telaCadastrarCardapio(void);
+void telaPesquisarCardapio(void);
+void telaAlterarCardapio(void);
+void telaExcluirCardapio(void);
+
+void CadastrarCardapio(void);
+void PesquisarCardapio(void);
+void AlterarCardapio(void);
+void ExcluirCardapio(void);
+
