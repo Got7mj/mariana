102c315b1c8e5d0561aad7ba427f42fd082693b6
Mariana <92758993+Got7mj@users.noreply.github.com>
2021-12-12 11:13:33 -0300

    Update relatorio.c

--- relatorio.c
+++ relatorio.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "relatorio.h"
+#include "biblioteca.h"
+
+void moduloRelatorio(void) {
+    char opção;
+    do {
+         opção = menuRelatorio();
+         switch(opção) {
+             case '1':  ?????();
+                        break;
+             case '2':  ?????();
+                        break;
+             case '3':  ?????();
+                        break;
+             case '4':  ??????();
+                        break;
+         }
+    }while (opcao != '0');
+}
 
